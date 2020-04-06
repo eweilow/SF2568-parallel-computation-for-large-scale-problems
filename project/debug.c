1639c6d578d56662fcfb6cd69262dbf958d8c109
Harald Melin <harald.melin@gmail.com>
2020-04-06 11:05:41 +0200

    added debug of uppdating all tiles

--- project/debug.c
+++ project/debug.c
@@ -17,7 +17,7 @@ void debugTiles(
 }
 
 
-void debugUpdateTiles(
+void debugUpdateTile(
   TileGeometry* geometry,
   long timestep
 ) {
@@ -69,4 +69,58 @@ void debugUpdateTiles(
       geometry->tiles[n].historicalData[timestep+1].vegetation
     );
   }
+}
+
+  void debugUpdateTiles(
+    TileGeometry* geometry,
+    long timestep
+  ) {
+
+    printf("\ndebug output at timestep %d\n", timestep);
+    printf("(%5s, %5s): %8s %8s %8s\n", "x", "y", "rabbits", "foxes", "veg.");
+    for(long n = 0; n < geometry->tileCount; n++) {
+      printf("(%5.2f, %5.2f): %8d %8d %8.2f\n",
+        geometry->tiles[n].x,
+        geometry->tiles[n].y,
+        geometry->tiles[n].historicalData[timestep].rabbitCount,
+        geometry->tiles[n].historicalData[timestep].foxCount,
+        geometry->tiles[n].historicalData[timestep].vegetation
+      );
+    }
+    printf("\n ########### updateTiles Start ######## timestep: %d\n", timestep);
+    int nTiles = geometry->tileCount;
+    for (int j=0; j<nTiles; j++) {
+      long *adjTilesIndPtr = geometry->tiles[j].adjacency.immediatelyAdjacentTileIndices;
+      long *indAdjTilesIndPtr = geometry->tiles[j].adjacency.indirectlyAdjacentTileIndices;
+      int size1 = geometry->tiles[j].adjacency.immediatelyAdjacentTileIndicesCount;
+      int size2 = geometry->tiles[j].adjacency.indirectlyAdjacentTileIndicesCount;
+      Tile imAdjTiles[size1];
+      Tile indAdjTiles[size2];
+
+      for (int i=0; i<size1; i++){
+        imAdjTiles[i] = geometry->tiles[adjTilesIndPtr[i]];
+      }
+      for (int i=0; i<size2; i++){
+        indAdjTiles[i] = geometry->tiles[indAdjTilesIndPtr[i]];
+      }
+
+      updateTile(
+        &(geometry->tiles[j]),
+        size1,
+        imAdjTiles,
+        size2,
+        indAdjTiles,
+        timestep
+      );
+    }
+    printf("\ndebug output at timestep %d\n", timestep+1);
+    for(long n = 0; n < geometry->tileCount; n++) {
+      printf("(%5.2f, %5.2f): %8d %8d %8.2f\n",
+        geometry->tiles[n].x,
+        geometry->tiles[n].y,
+        geometry->tiles[n].historicalData[timestep+1].rabbitCount,
+        geometry->tiles[n].historicalData[timestep+1].foxCount,
+        geometry->tiles[n].historicalData[timestep+1].vegetation
+      );
+    }
 }

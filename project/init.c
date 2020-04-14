02f17265ff4495d1f6719ecde22b502c3763abe9
Harald Melin <harald.melin@gmail.com>
2020-04-14 23:03:25 +0200

    begin expanding types

--- project/init.c
+++ project/init.c
@@ -1,3 +1,35 @@
+
+Rabbit initializeRabbit(long timestep){
+  Rabbit rabbit;
+  rabbit.birthDay = timestep;
+  return rabbit;
+}
+
+Rabbit* initializeRabbits(long nRabbits, long timestep){
+  Rabbit* rabbits = (Rabbit*) calloc(nRabbits, sizeof(Rabbit));
+  for(int i=0; i < nRabbits; i++) {
+    rabbits[i] = initializeRabbit(timestep);
+  }
+  return rabbits;
+}
+
+Fox initializeFox(long timestep) {
+  Fox fox;
+  fox.birthDay = timestep;
+  fox.mealTimeStamps = (long*) calloc(4, sizeof(long));
+  long emptyStomach[4] = {-1, -1, -1, -1};
+  fox.mealTimeStamps = emptyStomach;
+  return fox;
+}
+
+Fox* initializeFoxes(long nFoxes, long timestep) {
+  Fox* foxes = (Fox*) calloc(nFoxes, sizeof(Fox));
+  for(int i=0; i < nFoxes; i++) {
+    foxes[i] = initializeFox(timestep);
+  }
+  return foxes;
+}
+
 void initializeTile(
   Tile* tile,
   long timesteps
@@ -7,7 +39,9 @@ void initializeTile(
   TileData initialData;
 
   initialData.rabbitCount = 13;
+  initialData.rabbits = initializeRabbits(13, 0);
   initialData.foxCount = 7;
+  initialData.foxes = initializeFoxes(7, 0);
   initialData.vegetation = 0.25;
 
   tile->historicalData[0] = initialData;
--- project/types.c
+++ project/types.c
@@ -1,6 +1,17 @@
 typedef struct {
+  long birthDay;
+} Rabbit;
+
+typedef struct {
+  long birthDay;
+  long* mealTimeStamps;
+} Fox;
+
+typedef struct {
   long rabbitCount;
+  Rabbit* rabbits;
   long foxCount;
+  Fox* foxes;
   double vegetation;
 } TileData;
 

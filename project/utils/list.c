947290181a603d8fde7b04962bc7d0b05ff33dee
eweilow <eweilow@digitalkoppar.se>
2020-04-28 13:03:07 +0200

    Update list

--- project/utils/list.c
+++ project/utils/list.c
@@ -55,7 +55,7 @@ void list_increaseSize(
 ) {
   long newSizePower2 = next_pow2(newSize);
   if(DEBUG_LIST) {
-    printf("[LIST] Increasing size from %ld to %ld elements\n", list->currentMemorySize/list->elementSize, newSizePower2 / list->elementSize);
+    printf("[LIST] Increasing size from %ld elements (%ld bytes) to %ld elements (%ld bytes)\n", list->currentMemorySize/list->elementSize, list->currentMemorySize, newSizePower2 / list->elementSize, newSizePower2);
   }
   list->memptr = (void*) realloc(list->memptr, newSizePower2);
   list->currentMemorySize = newSizePower2;
@@ -65,9 +65,13 @@ void list_decreaseSize(
   List* list,
   size_t newSize
 ) {
-  long newSizePower2 = next_pow2(newSize);
+  long newSizePower2 = newSize == 0 ? 0 : next_pow2(newSize);
+  if(newSizePower2 == 1) {
+    printf("[LIST] Skipping list clear (new size too small) after moving from %ld elements (%ld bytes) to %ld elements (%ld bytes)\n", list->currentMemorySize/list->elementSize, list->currentMemorySize, newSizePower2 / list->elementSize, newSizePower2);
+    return;
+  }
   if(DEBUG_LIST) {
-    printf("[LIST] Decreasing size from %ld to %ld elements\n", list->currentMemorySize/list->elementSize, newSizePower2 / list->elementSize);
+    printf("[LIST] Decreasing size from %ld elements (%ld bytes) to %ld elements (%ld bytes)\n", list->currentMemorySize/list->elementSize, list->currentMemorySize, newSizePower2 / list->elementSize, newSizePower2);
   }
   list->memptr = (void*) realloc(list->memptr, newSizePower2);
   list->currentMemorySize = newSizePower2;
@@ -92,7 +96,7 @@ void list_insert(
   if(DEBUG_LIST) {
     printf("[LIST] Writing at location %ld (%ld bytes)\n", index, byteIndex);
   }
-  memmove(list->memptr + byteIndex, element, list->elementSize);
+  memcpy(list->memptr + byteIndex, element, list->elementSize);
   list->elementCount = list->elementCount + 1;
 }
 
@@ -120,18 +124,21 @@ void list_remove(
   long copyFromByteIndex = copyFromIndex * list->elementSize;
   long copyToByteIndex = copyToIndex * list->elementSize;
 
-  long length = list->elementCount - index;
+  long length = list->elementCount - copyFromIndex;
   long byteLength = length * list->elementSize;
 
   if(DEBUG_LIST) {
-    printf("[LIST] Moving %ld elements (%ld bytes) down one index from %ld (%ld bytes) to %ld (%ld bytes)\n", 
+    printf("[LIST] Removing element at %ld (@%ld bytes)\n", 
+      copyToIndex, copyToByteIndex
+    );
+    printf("[LIST] Moving %ld elements (%ld bytes) down one index from %ld (@%ld bytes) to %ld (@%ld bytes)\n", 
       length, byteLength, 
       copyFromIndex, copyFromByteIndex,
       copyToIndex, copyToByteIndex
     );
   }
   
-  memcpy(list->memptr + copyFromByteIndex, deleted, list->elementSize);
+  memcpy(deleted, list->memptr + copyToByteIndex, list->elementSize);
 
   memmove(list->memptr + copyToByteIndex, list->memptr + copyFromByteIndex, byteLength);
   list->elementCount = list->elementCount - 1;
@@ -156,4 +163,26 @@ void list_read(
 ) {
   list_count(list, count);
   *ptr = list->memptr;
+}
+
+
+List list_clone(
+  List* list
+) {
+  if(DEBUG_LIST) {
+    printf("[LIST] Copying list of size %ld (%ld bytes)\n", 
+      list->elementCount,
+      list->currentMemorySize
+    );
+  }
+  List cloned;
+
+  cloned.memptr = (void*)malloc(list->currentMemorySize);
+  memcpy(cloned.memptr, list->memptr, list->currentMemorySize);
+  
+  cloned.elementSize = list->elementSize;
+  cloned.elementCount = list->elementCount;
+  cloned.currentMemorySize = list->currentMemorySize;
+
+  return cloned;
 }

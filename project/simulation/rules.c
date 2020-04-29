14f5452b623b34cf24acf4e8653a404d898bb697
Harald Melin <haraldme@kth.se>
2020-04-29 16:08:09 +0200

    Simple rule added

--- project/simulation/rules.c
+++ project/simulation/rules.c
@@ -5,12 +5,16 @@
 //       long number of rabbits at startOfNewDay
 // - out: long vegetationAtEndOfDay
 double vegetationAtEndOfDayRule(double vegetationAtStartOfDay, long nRabbitsAtStartOfDay) {
-  return 1.10*vegetationAtStartOfDay - 0.001*(double)nRabbitsAtStartOfDay;
+  double vegetationAtEndOfDay = 1.1*(vegetationStart - 0.001*rabbitCountStart);
+  vegetationAtEndOfDay = vegetationAtEndOfDay > 1.0 ? 1.0 : vegetationAtEndOfDay;
+  vegetationAtEndOfDay = vegetationAtEndOfDay < 0.1 ? 0.1 : vegetationAtEndOfDay;
+  return vegetationAtEndOfDay;
 }
 
-
 ///////////////// RABBITS ///////////////////////
-
+long numberOfRabbitsToMigrateAtEndOfDayRule(long numberOfRabbitsAtEndOfDay){
+  return numberOfRabbitsToMigrate / 5;
+}
 
 
 

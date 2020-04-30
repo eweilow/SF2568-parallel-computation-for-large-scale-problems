f86e8bea234a89128f75f346dc8591d8cc146a23
eweilow <eweilow@digitalkoppar.se>
2020-04-30 14:25:15 +0200

    Reference implementation

--- project/simulation/init.c
+++ project/simulation/init.c
@@ -2,19 +2,24 @@ void initializeTile(
   Tile* tile,
   long timesteps
 ) {
-  tile->historicalData = (TileData*) calloc(timesteps, sizeof(TileData));
+  if(tile->isOwnedByThisProcess) {
+    tile->historicalDataCount = timesteps;
+    tile->historicalData = (TileData*) calloc(timesteps, sizeof(TileData));
 
-  tile->historicalData[0].rabbitsList = initList(sizeof(Rabbit), 0);
-  for(long l = 0; l < 13; l++) {
-    birthRabbit(tile, 0);
-  }
+    tile->historicalData[0].rabbitsList = initList(sizeof(Rabbit), 0);
+    tile->historicalData[0].foxesList = initList(sizeof(Fox), 0);
 
-  tile->historicalData[0].foxesList = initList(sizeof(Fox), 0);
-  for(long l = 0; l < 7; l++) {
-    birthFox(tile, 0);
+    if(!tile->isWaterTile) {
+      for(long l = 0; l < 13; l++) {
+        birthRabbit(tile, 0);
+      }
+      for(long l = 0; l < 7; l++) {
+        birthFox(tile, 0);
+      }
+        
+      tile->historicalData[0].vegetation = 0.25;
+    }
   }
-    
-  tile->historicalData[0].vegetation = 0.25;
 
   tile->rabbitMigrationsList = initList(sizeof(RabbitMigration), 0);
   tile->foxMigrationsList = initList(sizeof(FoxMigration), 0);
